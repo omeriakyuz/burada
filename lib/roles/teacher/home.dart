cb29453c65f6a58980834c7da3e8444e95dad938
omeriakyuz <omerakyuz5178@gmail.com>
2024-05-20 05:14:04 +0300

    liste revize edildi. esp32 değişkenleri kaldırıldı. BLE fonksiyonları için mocking eklendi

--- lib/roles/teacher/home.dart
+++ lib/roles/teacher/home.dart
@@ -5,7 +5,6 @@ import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:burada/colors.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:http/http.dart' as http;
 
 class TeacherHomePage extends StatefulWidget {
   const TeacherHomePage({Key? key});
@@ -18,172 +17,94 @@ class _TeacherHomePageState extends State<TeacherHomePage> {
   final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
   int _currentPageIndex = 0;
   List<String> buttonTexts = [];
-  bool classStarted = false;
-  final String esp32IP = '192.168.1.81';
-  String teacherName = currentTeacher; 
+  Map<String, bool> classStartedStates = {};
+  String teacherName = currentTeacher;
 
   @override
   Widget build(BuildContext context) {
     return TeacherBasePage(
       title: 'Kontrol Paneli',
       currentPageIndex: _currentPageIndex,
-      buildBody: (context) => StreamBuilder<QuerySnapshot>(
-          stream: FirebaseFirestore.instance
-              .collection('subjects')
-              .where('teacherName', isEqualTo: teacherName)
-              .snapshots(),
-          builder: (context, snapshot) {
-            if (snapshot.hasError) {
-              return Center(
-                child: Text('Error: ${snapshot.error}'),
-              );
-            }
+      buildBody: (context) =>
+          StreamBuilder<QuerySnapshot>(
+              stream: FirebaseFirestore.instance
+                  .collection('subjects')
+                  .where('teacherName', isEqualTo: teacherName)
+                  .snapshots(),
+              builder: (context, snapshot) {
+                if (snapshot.hasError) {
+                  return Center(
+                    child: Text('Hata: ${snapshot.error}'),
+                  );
+                }
 
-            if (snapshot.connectionState == ConnectionState.waiting) {
-              return const Center(
-                child: CircularProgressIndicator(),
-              );
-            }
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return const Center(
+                    child: CircularProgressIndicator(),
+                  );
+                }
 
-            final List<DocumentSnapshot> documents = snapshot.data!.docs;
+                final List<DocumentSnapshot> documents = snapshot.data!.docs;
 
-            return SingleChildScrollView(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.start,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  const SizedBox(height: 15),
-                  Container(
-                    padding: const EdgeInsets.all(5.0),
-                    child: Column(
-                      children: documents.map((document) {
-                        final subjectName = document['subject'] as String;
-                        final index = documents.indexOf(document);
-                        while (buttonTexts.length <= index) {
-                          buttonTexts.add('Start Class');
-                        }
-                        return Padding(
-                          padding: const EdgeInsets.only(
-                            top: 8,
-                            bottom: 8,
-                            right: 10,
-                            left: 10,
-                          ),
-                          child: Container(
-                            padding: const EdgeInsets.all(5),
-                            width: double.infinity,
-                            decoration: BoxDecoration(
-                              border: Border.all(color: lightest),
-                              borderRadius: BorderRadius.circular(15),
-                              color: lightest,
+                return ListView.builder(
+                  itemCount: documents.length,
+                  itemBuilder: (context, index) {
+                    final subjectName = documents[index]['subject'] as String;
+                    bool isClassStarted = classStartedStates[subjectName] ??
+                        false; // Get state from map
+
+                    return Padding(
+                      padding: const EdgeInsets.all(4.0),
+                      child: Card(
+                        color: lightest,
+                        elevation: 2,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(12),
+                      ),
+                        child: ListTile(
+                          title: Text(subjectName, style: TextStyle(fontSize: 20)),
+                          trailing: ElevatedButton(
+                            onPressed: () {
+                              setState(() {
+                                classStartedStates[subjectName] =
+                                !isClassStarted;
+                              });
+                        
+                              if (isClassStarted) {
+                                stopBLEScan();
+                              } else {
+                                startBLEScan();
+                              }
+                            },
+                            style: ElevatedButton.styleFrom(
+                              backgroundColor: middle,
+                              textStyle: GoogleFonts.nunito(
+                                  fontSize: 15, color: Colors.black),
                             ),
-                            child: Column(
-                              children: [
-                                Align(
-                                  alignment: Alignment.centerLeft,
-                                  child: Padding(
-                                    padding:
-                                        const EdgeInsets.only(left: 10, top: 5),
-                                    child: Text(
-                                      subjectName,
-                                      style: const TextStyle(
-                                          fontSize: 20, color: Colors.black),
-                                      textAlign: TextAlign.start,
-                                    ),
-                                  ),
+                            child: Text(
+                                isClassStarted ? 'Yoklamayı Durdur' : 'Yoklamayı Başlat',
+                                style: const TextStyle(
+                                color: Colors.black
                                 ),
-                                const SizedBox(height: 5),
-                                Align(
-                                  alignment: Alignment.centerRight,
-                                  child: ElevatedButton(
-                                    onPressed: () {
-                                      final isStartPressed =
-                                          buttonTexts[index] == 'Start Class';
-                                      if (classStarted) {
-                                        stopBLEScan();
-                                      } else {
-                                        startBLEScan();
-                                      }
-                                      ScaffoldMessenger.of(context)
-                                          .showSnackBar(
-                                        SnackBar(
-                                          content: Row(
-                                            children: [
-                                              Icon(Icons.check,
-                                                  color: Colors.white),
-                                              SizedBox(width: 8),
-                                              Text(
-                                                isStartPressed
-                                                    ? 'Class Started Successfully'
-                                                    : 'Class Ended Successfully',
-                                              ),
-                                            ],
-                                          ),
-                                          backgroundColor: Colors.green,
-                                        ),
-                                      );
+                              ),
 
-                                      setState(() {
-                                        classStarted = !classStarted;
-                                        buttonTexts[index] = classStarted
-                                            ? 'End Class'
-                                            : 'Start Class';
-                                      });
-                                    },
-                                    style: ElevatedButton.styleFrom(
-                                      backgroundColor: middle,
-                                      textStyle: GoogleFonts.nunito(
-                                          fontSize: 20, color: Colors.black),
-                                    ),
-                                    child: Text(
-                                      classStarted
-                                          ? 'End Class'
-                                          : 'Start Class',
-                                      style: TextStyle(color: Colors.black),
-                                    ),
-                                  ),
-                                ),
-                              ],
-                            ),
                           ),
-                        );
-                      }).toList(),
-                    ),
-                  ),
-                ],
-              ),
-            );
-          }),
+                        ),
+                      ),
+                    );
+                  },
+                );
+              }),
     );
   }
 
-  void startBLEScan() async {
-    try {
-      final response = await http.post(
-        Uri.parse('http://$esp32IP/startScan'),
-      );
-      print('Scanning');
-      setState(() {
-        classStarted = true;
-      });
-      print('Response: ${response.statusCode} ${response.body}');
-    } catch (e) {
-      print('Error starting BLE scan: $e');
-    }
+  void startBLEScan() {
+    print('tarama başladı.');
+
   }
 
-  void stopBLEScan() async {
-    try {
-      final response = await http.post(
-        Uri.parse('http://$esp32IP/stopScan'),
-      );
-      print('Stopping');
-      setState(() {
-        classStarted = false;
-      });
-      print('Response: ${response.statusCode} ${response.body}');
-    } catch (e) {
-      print('Error stopping BLE scan: $e');
-    }
+  void stopBLEScan() {
+    print('tarama durdu.');
+
   }
 }
