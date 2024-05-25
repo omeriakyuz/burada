b115883d3f45ec4b76377e9c1b298b5d18ed81c5
omeriakyuz <omerakyuz5178@gmail.com>
2024-05-25 06:20:22 +0300

    scan fonksiyonları ile alakalı denemeler yapıldı

--- lib/roles/teacher/home.dart
+++ lib/roles/teacher/home.dart
@@ -1,10 +1,18 @@
+import 'dart:async';
+import 'dart:convert';
+import 'dart:typed_data';
+
 import 'package:burada/roles/teacher/email_auth.dart';
 import 'package:burada/roles/teacher/teacher_base.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:burada/colors.dart';
+import 'package:flutter_ble_peripheral/flutter_ble_peripheral.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:flutter_blue_plus/flutter_blue_plus.dart';
+import 'package:flutter_ble_peripheral/flutter_ble_peripheral.dart';
+
 
 class TeacherHomePage extends StatefulWidget {
   const TeacherHomePage({Key? key});
@@ -19,6 +27,11 @@ class _TeacherHomePageState extends State<TeacherHomePage> {
   List<String> buttonTexts = [];
   Map<String, bool> classStartedStates = {};
   String teacherName = currentTeacher;
+  List<ScanResult> _scanResults = [];
+  late StreamSubscription<List<ScanResult>> _scanResultsSubscription;
+  late StreamSubscription<bool> _isScanningSubscription;
+  bool _isScanning = false;
+
 
   @override
   Widget build(BuildContext context) {
@@ -72,6 +85,7 @@ class _TeacherHomePageState extends State<TeacherHomePage> {
                         
                               if (isClassStarted) {
                                 stopBLEScan();
+                                resultHandling();
                               } else {
                                 startBLEScan();
                               }
@@ -98,13 +112,44 @@ class _TeacherHomePageState extends State<TeacherHomePage> {
     );
   }
 
-  void startBLEScan() {
+  Future<void> startBLEScan() async {
     print('tarama başladı.');
+    var subscription = FlutterBluePlus.onScanResults.listen((results) {
+      if (results.isNotEmpty) {
+        ScanResult r = results.last; // the most recently found device
+//        print('${r.device.remoteId}: "${r.advertisementData.manufacturerData}" bulundu!');
+        List<int> codeUnits = [116,101,115,116]; //ascii / utf-8 (temel harfler için aynı)
+        String decodedText = utf8.decode(codeUnits);
+
+        print(decodedText); // Çıktı: 5$,"\x9B±
+      }
+    },
+      onError: (e) => print(e),
+    );
+
+// cleanup: cancel subscription when scanning stops
+    FlutterBluePlus.cancelWhenScanComplete(subscription);
+
+// Wait for Bluetooth enabled & permission granted
+// In your real app you should use `FlutterBluePlus.adapterState.listen` to handle all states
+    await FlutterBluePlus.adapterState.where((val) => val == BluetoothAdapterState.on).first;
+
+// Start scanning w/ timeout
+// Optional: use `stopScan()` as an alternative to timeout
+    await FlutterBluePlus.startScan(
+        timeout: Duration(seconds:15));
+
+// wait for scanning to stop
+    await FlutterBluePlus.isScanning.where((val) => val == false).first;
+  }
+
+  void resultHandling() {
 
   }
 
-  void stopBLEScan() {
+  Future<void> stopBLEScan() async {
     print('tarama durdu.');
+    await FlutterBluePlus.stopScan();
 
   }
 }
